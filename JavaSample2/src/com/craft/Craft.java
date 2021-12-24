f59b0fd3a2cae4e9e313c53b70c9388ce3bae644
ZKidane <zerezghi.kidane@gmail.com>
2021-12-24 13:50:18 -0500

    commit from eclipse

---
+++ JavaSample2/src/com/craft/Craft.java
@@ -0,0 +1,16 @@
+package com.craft;
+
+public class Craft {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("print somthing");
+        System.out.println("print somthing"); 
+        System.out.println("print somthing"); 
+        System.out.println("print somthing");
+        System.out.println("print somthing"); 
+        System.out.println("print somthing"); 
+	}
+
+}
