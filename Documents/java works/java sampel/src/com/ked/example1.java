b2b5dc95ba5e10d721362be9fc743b2e5e12d09c
kedir <kedir@10.0.0.217>
2021-12-25 11:38:59 -0500

    commit from eclipse

---
+++ Documents/java works/java sampel/src/com/ked/example1.java
@@ -0,0 +1,17 @@
+package com.ked;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.setOut(null);
+		System.setOut(null);System.setOut(null);System.setOut(null);
+		System.setOut(null);
+		System.setOut(null);
+		System.setOut(null);
+		System.setOut(null);
+	}
+
+	
+}
