3e765c74041404f58b04a727dd5d4a74bcef676d
saraGITHUB1 <mareshetninu@yahoo.com>
2021-12-28 14:51:07 -0500

    commit gena

---
+++ dec javvva/src/meskel/gena.java
@@ -0,0 +1,12 @@
+package meskel;
+
+public class gena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("holiday");
+		System.out.println("holiday");
+		
+	}
+
+}
