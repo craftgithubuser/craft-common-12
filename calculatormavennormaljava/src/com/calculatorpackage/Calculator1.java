8d6e2c3dea9265554aad64799f9689c40fc02eff
SARAGITHUB1 <mareshetninu@yahoo.com>
2022-02-11 16:35:04 -0500

    message

---
+++ calculatormavennormaljava/src/com/calculatorpackage/Calculator1.java
@@ -0,0 +1,20 @@
+package com.calculatorpackage;
+
+public class Calculator1 {
+
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	public int muliplication(int a,int b) {
+		return  a*b;
+		
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator1 c =new Calculator1();
+		System.out.println();
+		
+	}
+
+}
