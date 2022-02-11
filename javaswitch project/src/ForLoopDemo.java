4e0058dfa88b7cb693814c603c3e2a309b2c9b0b
SARAGITHUB1 <mareshetninu@yahoo.com>
2022-02-11 12:58:58 -0500

    commit commit today

---
+++ javaswitch project/src/ForLoopDemo.java
@@ -0,0 +1,35 @@
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//     int i=0;
+//		for(i=0;i<=10;i++) {
+//			System.out.print(i+ " ");
+//		}
+//		
+//		
+//				int j=0;  //second eg.
+//		for(j=0;j<=5;j++) {
+//		System.out.println(j);
+//	}
+		
+//		int sum=0;
+//		for (int i=1;i<=50;i++) {
+//		sum=sum+i;
+//		System.out.println(sum);
+//		}
+//			System.out.println(i + " is even");
+//		}
+//			else {
+//				System.out.println(i + " is odd");
+//			}
+		int n=1;
+		n=2;
+		for(int i=1;i<=10;i++) {
+			
+			{  System.out.println(n+"*"+i+"="+n*i); 
+		}
+	}
+}
+}
---
+++ javaswitch project/src/Operators.java
@@ -0,0 +1,21 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int a =20;
+//int b=(a==1)?30:40;
+////20=1 is false so the value will be after column :40
+//System.out.println("value of b is :"+ b);	
+//	
+//	b=(a==20)? 30:40; //the first condn. is true and the value will be 1st expression 30
+//	System.out.println("the value of b is : "+b);
+//		
+	int age =20;    //ternary condition
+	String result;
+	result =(age>=18)? "you are in":"you are not in";
+	System.out.println(result);
+	
+	}
+
+}
